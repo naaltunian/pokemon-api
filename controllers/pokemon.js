162a57d5d14585a0f5180edb3b2214165431237b
naaltunian <naaltunian@icloud.com>
2024-03-24 14:06:12 -0400

    added findById route'

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -11,4 +11,15 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:pokemonId', async (req, res) => {
+    try {
+        const { pokemonId } = req.params
+        const pokemon = await Pokemon.find({ pokemonId })
+        res.json(pokemon)
+    } catch (error) {
+        console.log('Error fetching pokemon by id', error)
+        res.status(500).json({ message: 'error fetching pokemon by id' })
+    }
+})
+
 module.exports = router;
